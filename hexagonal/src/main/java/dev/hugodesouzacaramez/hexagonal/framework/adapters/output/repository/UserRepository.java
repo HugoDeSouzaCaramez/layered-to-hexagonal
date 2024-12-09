038b4de1f1d10a5bda00d3ec3b774330d78fe24f
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-12-09 17:44:36 -0300

    adicionando arquivos faltantes no hexagono framework

---
+++ hexagonal/src/main/java/dev/hugodesouzacaramez/hexagonal/framework/adapters/output/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package dev.hugodesouzacaramez.hexagonal.framework.adapters.output.repository;
+
+import dev.hugodesouzacaramez.hexagonal.framework.adapters.output.data.UserData;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.Optional;
+
+@ApplicationScoped
+public class UserRepository implements PanacheRepository<UserData> {
+
+    public Optional<UserData> findByEmail(String email) {
+        return find("email", email).firstResultOptional();
+    }
+}
